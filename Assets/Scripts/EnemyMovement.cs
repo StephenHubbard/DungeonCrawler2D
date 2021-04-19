0683e0d4be56f695f7765e74b79efba529b05740
Stephen Hubbard <hubbardgames@gmail.com>
2021-04-19 10:50:51 -0600

    enemy movement colliders and damage ui

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Transform movePoint;
+    [SerializeField] LayerMask whatStopsMovement;
+    [SerializeField] private int powerLevel = 1;
+
+    private void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        int randomDir = Random.Range(-1, 2);
+        int randomAxis = Random.Range(-1, 2);
+
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f || Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+        {
+            if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+            {
+                if (randomAxis < 0)
+                {
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(randomDir, 0f, 0f), .2f, whatStopsMovement))
+                    {
+                        movePoint.position += new Vector3(randomDir, 0f, 0f);
+                    }
+                }
+                else if (randomAxis > 0)
+                {
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, randomDir, 0f), .2f, whatStopsMovement))
+                    {
+                        movePoint.position += new Vector3(0f, randomDir, 0f);
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.GetComponent<Health>().TakeDamage(powerLevel);
+        }
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int startingHearts;
+    [SerializeField] private int currentHearts;
+    [SerializeField] private Transform heartsContainer;
+
+    private void Awake()
+    {
+        currentHearts = startingHearts;
+    }
+
+    private void Start()
+    {
+        UpdateHeartsUI();
+    }
+
+    private void UpdateHeartsUI()
+    {
+        for (int i = 0; i < heartsContainer.childCount; i++)
+        {
+            heartsContainer.GetChild(i).gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i < currentHearts; i++)
+        {
+            heartsContainer.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHearts -= damage;
+
+        UpdateHeartsUI();
+    }
+}
